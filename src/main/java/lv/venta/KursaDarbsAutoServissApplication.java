a587dc9eafc169e55bad2b2888efb95dc1847ec5
Sandis Kaģis <s20kagisand@venta.lv>
2024-05-08 20:02:17 +0300

    New: project

---
+++ src/main/java/lv/venta/KursaDarbsAutoServissApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursaDarbsAutoServissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursaDarbsAutoServissApplication.class, args);
+	}
+
+}
