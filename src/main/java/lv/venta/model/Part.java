ac39cd1029f894fb265c95cabedfaf066d8efee9
Sandis Kaģis <s20kagisand@venta.lv>
2024-05-08 22:10:19 +0300

    New classes: Part, Lights, Tire and enums

---
+++ src/main/java/lv/venta/model/Part.java
@@ -0,0 +1,51 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "PartTable")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Part {
+
+	@Column(name = "idP")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idP;
+	
+	@Column(name = "price")
+	@NotNull
+	@DecimalMin(value = "1.0")
+	@DecimalMax(value = "1000.0")
+	private float price;
+	
+	@Column(name = "manufacturer")
+	@NotNull
+	@Size(min = 2, max = 40)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String manufacturer;
+	
+	public Part(float price, String manufacturer) {
+		setPrice(price);
+		setManufacturer(manufacturer);
+	}
+	
+}
